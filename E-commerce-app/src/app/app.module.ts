7cc6313800db35d37f3e05896cbe11b08d96fa23
Pepito <kathleen.mae.pepito@accenture.com>
2023-06-20 16:45:17 +0800

    update files

--- E-commerce-app/src/app/app.module.ts
+++ E-commerce-app/src/app/app.module.ts
@@ -3,22 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { HeaderComponent } from './header/header.component';
 import { ProductsComponent } from './products/products.component';
-=======
 import { HomeComponent } from './home/home.component';
->>>>>>> member
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     HeaderComponent,
-    ProductsComponent
-=======
+    ProductsComponent,
     HomeComponent
->>>>>>> member
+
   ],
   imports: [
     BrowserModule,
