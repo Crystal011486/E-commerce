f123f9f2ae88485aa3eaba57ca95b0c33d21880f
Villamor, Cristalyn <c.c.kapangyarihan@accenture.com>
2023-06-21 16:36:48 +0800

    created footer component, updated the app routing module,added logo image on images folder

--- E-commerce-app/src/app/app.module.ts
+++ E-commerce-app/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { ProductsComponent } from './products/products.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HomeComponent } from './home/home.component';
     HeaderComponent,
     ProductsComponent,
     LandingPageComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
 
   ],
   imports: [
